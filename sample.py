00ee52a7343c5a5d8ae5924db00e80bd31dab1ed
EC2 Default User <ec2-user@ip-172-31-43-194.ap-northeast-1.compute.internal>
2022-12-06 10:13:57 +0000

    sendil

---
+++ sample.py
@@ -0,0 +1,23 @@
+#int
+#float
+#string
+#complex
+
+x = 5
+print(x)
+print(type(x))
+
+y = 20.2
+print(y)
+print(type(y))
+
+z = 'sendil'
+print(z)
+print(type(z))
+
+A = 1+4j
+print(A)
+print(type(A))
+
+
+
