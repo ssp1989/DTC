d944c83027674ef48dd8e9499db782821e509059
EC2 Default User <ec2-user@ip-172-31-43-194.ap-northeast-1.compute.internal>
2022-12-06 13:56:03 +0000

    datatypes

--- sample.py
+++ sample.py
@@ -19,5 +19,67 @@ A = 1+4j
 print(A)
 print(type(A))
 
+# Conversion of data types
+
+a = float(x)
+print(a)
+print(type(a))
+
+b = int(y)
+print(b)
+print(type(b))
+
+c = complex(x)
+print(c)
+print(type(c))
+
+#LIST
+
+d = ['sendil','velu','vignesh','maheen']
+print(d)
+print(type(d))
+
+#TUPEL
+
+e = ('apple','orange','pine apple')
+print(e)
+print(type(e))
+
+#SET
+
+f = {'pulsar','rx100','bullet'}
+print(f)
+print(type(f))
+
+#DICTIONARY
+
+g = {'name':'sendil','age':32}
+print(g)
+print(type(g))
+
+#FROZEN SET
+
+h = frozenset ({'apple','orange','water melon'})
+print(h)
+print(type(h))
+
+#RANGE
+
+i = range(5)
+print(i)
+print(type(i))
+
+#BOOL
+
+j = True
+print(j)
+print(type(j))
+
+k = False
+print(k)
+print(type(k))
+
+
+
 
 
