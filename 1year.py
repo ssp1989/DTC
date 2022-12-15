b6460f72183abd67253e336a43351c9d7a3c56bf
EC2 Default User <ec2-user@ip-172-31-35-99.ap-northeast-1.compute.internal>
2022-12-15 10:48:56 +0000

    8 semester

--- 1year.py
+++ 1year.py
@@ -77,8 +77,236 @@ else:
     else:
         print('No Grade')
 
+print('Semester 3 :')
 
-cgpa = (gpa + gpa_2)/2
+s3_t1 = int(input('enter the mark of s3_t1 :'))
+s3_t2 = int(input('enter the mark of s3_t2 :'))
+s3_t3 = int(input('enter the mark of s3_t3 :'))
+s3_t4 = int(input('enter the mark of s3_t4 :'))
+s3_t5 = int(input('enter the mark of s3_t5 :'))
+s3_t6 = int(input('enter the mark of s3_t6 :'))
+s3_l1 = int(input('enter the mark of s3_l1 :'))
+s3_l2 = int(input('enter the mark of s3_l2 :'))
+
+total3 = s3_t1 + s3_t2 + s3_t3 + s3_t4 + s3_t5 + s3_t6 + s3_l1 + s3_l2
+
+print('Total3 :' , total3)
+
+gpa_3 = total3/8
+
+print('GPA_3 :' , gpa_3)
+
+if s3_t1 >= 35 and s3_t2 >= 35 and s3_t3 >= 35 and s3_t4 >= 35 and s3_t5 >= 35 and s3_t6 >= 35 and s3_l1 >= 35 and s3_l2 >= 35:
+    print('Result : Pass')
+else:
+    print('Result : Fail')
+
+    if gpa_3 >= 90 and gpa_3 <= 100:
+        print('Grade : A')
+
+    elif gpa_3 >= 80 and gpa_3 <= 89:
+        print('Grade : B')
+
+    elif gpa_3 >= 70 and gpa_3 <= 79:
+        print('Grade : C')
+
+    elif gpa_3 <= 60:
+        print('Grade : D')
+
+    else:
+        print('No Grade')
+    
+print('Semester 4 :')
+
+s4_t1 = int(input('enter the mark of s4_t1 :'))
+s4_t2 = int(input('enter the mark of s4_t2 :'))
+s4_t3 = int(input('enter the mark of s4_t3 :'))
+s4_t4 = int(input('enter the mark of s4_t4 :'))
+s4_t5 = int(input('enter the mark of s4_t5 :'))
+s4_t6 = int(input('enter the mark of s4_t6 :'))
+s4_l1 = int(input('enter the mark of s4_l1 :'))
+s4_l2 = int(input('enter the mark of s4_l2 :'))
+
+total4 = s4_t1 + s4_t2 + s4_t3 + s4_t4 + s4_t5 + s4_t6 + s4_l1 + s4_l2
+
+print('Total4 :' , total4)
+
+gpa_4 = total4/8
+
+print('GPA_4 :' , gpa_4)
+
+if s4_t1 >= 35 and s4_t2 >= 35 and s4_t3 >= 35 and s4_t4 >= 35 and s4_t5 >= 35 and s4_t6 >= 35 and s4_l1 >= 35 and s4_l2 >= 35:
+    print('Result : Pass')
+else:
+    print('Result : Fail')
+
+    if gpa_4 >= 90 and gpa_4 <= 100:
+        print('Grade : A')
+
+    elif gpa_4 >= 80 and gpa_4 <= 89:
+        print('Grade : B')
+
+    elif gpa_4 >= 70 and gpa_4 <= 79:
+        print('Grade : C')
+
+    elif gpa_4 <= 60:
+        print('Grade : D')
+
+    else:
+        print('No Grade')
+
+print('Semester 5 :')
+
+s5_t1 = int(input('enter the mark of s5_t1 :'))
+s5_t2 = int(input('enter the mark of s5_t2 :'))
+s5_t3 = int(input('enter the mark of s5_t3 :'))
+s5_t4 = int(input('enter the mark of s5_t4 :'))
+s5_t5 = int(input('enter the mark of s5_t5 :'))
+s5_t6 = int(input('enter the mark of s5_t6 :'))
+s5_l1 = int(input('enter the mark of s5_l1 :'))
+s5_l2 = int(input('enter the mark of s5_l2 :'))
+
+total5 = s5_t1 + s5_t2 + s5_t3 + s5_t4 + s5_t5 + s5_t6 + s5_l1 + s5_l2
+
+print('Total5 :' , total5)
+
+gpa_5 = total5/8
+
+print('GPA_5 :' , gpa_5)
+
+if s5_t1 >= 35 and s5_t2 >= 35 and s5_t3 >= 35 and s5_t4 >= 35 and s5_t5 >= 35 and s5_t6 >= 35 and s5_l1 >= 35 and s5_l2 >= 35:
+    print('Result : Pass')
+else:
+    print('Result : Fail')
+
+    if gpa_5 >= 90 and gpa_5 <= 100:
+        print('Grade : A')
+
+    elif gpa_5 >= 80 and gpa_5 <= 89:
+        print('Grade : B')
+
+    elif gpa_5 >= 70 and gpa_5 <= 79:
+        print('Grade : C')
+
+    elif gpa_5 <= 60:
+        print('Grade : D')
+
+    else:
+        print('No Grade')
+
+print('Semester 6 :')
+
+s6_t1 = int(input('enter the mark of s6_t1 :'))
+s6_t2 = int(input('enter the mark of s6_t2 :'))
+s6_t3 = int(input('enter the mark of s6_t3 :'))
+s6_t4 = int(input('enter the mark of s6_t4 :'))
+s6_t5 = int(input('enter the mark of s6_t5 :'))
+s6_t6 = int(input('enter the mark of s6_t6 :'))
+s6_l1 = int(input('enter the mark of s6_l1 :'))
+s6_l2 = int(input('enter the mark of s6_l2 :'))
+
+total6 = s6_t1 + s6_t2 + s6_t3 + s6_t4 + s6_t5 + s6_t6 + s6_l1 + s6_l2
+
+print('Total6 :' , total6)
+
+gpa_6 = total6/8
+
+print('GPA_6 :' , gpa_6)
+
+if s6_t1 >= 35 and s6_t2 >= 35 and s6_t3 >= 35 and s6_t4 >= 35 and s6_t5 >= 35 and s6_t6 >= 35 and s6_l1 >= 35 and s6_l2 >= 35:
+    print('Result : Pass')
+else:
+    print('Result : Fail')
+
+    if gpa_6 >= 90 and gpa_6 <= 100:
+        print('Grade : A')
+
+    elif gpa_6 >= 80 and gpa_6 <= 89:
+        print('Grade : B')
+
+    elif gpa_6 >= 70 and gpa_6 <= 79:
+        print('Grade : C')
+
+    elif gpa_6 <= 60:
+        print('Grade : D')
+
+    else:
+        print('No Grade')
+
+
+print('Semester 7 :')
+
+s7_t1 = int(input('enter the mark of s7_t1 :'))
+s7_t2 = int(input('enter the mark of s7_t2 :'))
+s7_t3 = int(input('enter the mark of s7_t3 :'))
+s7_t4 = int(input('enter the mark of s7_t4 :'))
+s7_l1 = int(input('enter the mark of s7_l1 :'))
+s7_pp1 = int(input('enter the mark of s7_pp1 :'))
+
+total7 = s7_t1 + s7_t2 + s7_t3 + s7_t4 + s7_l1 + s7_pp1
+
+print('Total7 :' , total7)
+
+gpa_7 = total7/6
+
+print('GPA_7 :' , gpa_7)
+
+if s7_t1 >= 35 and s7_t2 >= 35 and s7_t3 >= 35 and s7_t4 >= 35 and s7_l1 >= 35 and s7_pp1 >= 35:
+    print('Result : Pass')
+else:
+    print('Result : Fail')
+
+    if gpa_7 >= 90 and gpa_7 <= 100:
+        print('Grade : A')
+
+    elif gpa_7 >= 80 and gpa_7 <= 89:
+        print('Grade : B')
+
+    elif gpa_7 >= 70 and gpa_7 <= 79:
+        print('Grade : C')
+
+    elif gpa_7 <= 60:
+        print('Grade : D')
+
+    else:
+        print('No Grade')
+
+print('Semester 8 :')
+
+s8_t1 = int(input('enter the mark of s8_t1 :'))
+s8_t2 = int(input('enter the mark of s8_t2 :'))
+s8_pp2 = int(input('enter the mark of s8_pp2 :'))
+
+total8 = s8_t1 + s8_t2 + s8_pp2 
+
+print('Total8 :' , total8)
+
+gpa_8 = total8/3
+
+print('GPA_8 :' , gpa_8)
+
+if s8_t1 >= 35 and s8_t2 >= 35 and s8_pp2 >= 35:
+    print('Result : Pass')
+else:
+    print('Result : Fail')
+
+    if gpa_8 >= 90 and gpa_8 <= 100:
+        print('Grade : A')
+
+    elif gpa_8 >= 80 and gpa_8 <= 89:
+        print('Grade : B')
+
+    elif gpa_8 >= 70 and gpa_8 <= 79:
+        print('Grade : C')
+
+    elif gpa_8 <= 60:
+        print('Grade : D')
+
+    else:
+        print('No Grade')
+
+
+cgpa = (gpa + gpa_2 + gpa_3 + gpa_4 + gpa_5 + gpa_6 + gpa_7 + gpa_8)/8
 
 print('CGPA :' , cgpa)
 
