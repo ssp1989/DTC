033a26bc6330e6946f016d4746eb76ed2d2b8d69
EC2 Default User <ec2-user@ip-172-31-44-191.ap-northeast-1.compute.internal>
2022-12-15 15:01:02 +0000

    compression of coding

---
+++ 8sem.py
@@ -0,0 +1,115 @@
+print('semester 1 :')
+
+s1_t1 = int(input('enter the mark of s1_t1 :'))
+s1_t2 = int(input('enter the mark of s1_t2 :'))
+s1_t3 = int(input('enter the mark of s1_t3 :'))
+s1_t4 = int(input('enter the mark of s1_t4 :'))
+s1_t5 = int(input('enter the mark of s1_t5 :'))
+s1_t6 = int(input('enter the mark of s1_t6 :'))
+s1_l1 = int(input('enter the mark of s1_l1 :'))
+s1_l2 = int(input('enter the mark of s1_l2 :'))
+
+print('semester 2 :')
+
+s2_t1 = int(input('enter the mark of s2_t1 :'))
+s2_t2 = int(input('enter the mark of s2_t2 :'))
+s2_t3 = int(input('enter the mark of s2_t3 :'))
+s2_t4 = int(input('enter the mark of s2_t4 :'))
+s2_t5 = int(input('enter the mark of s2_t5 :'))
+s2_t6 = int(input('enter the mark of s2_t6 :'))
+s2_l1 = int(input('enter the mark of s2_l1 :'))
+s2_l2 = int(input('enter the mark of s2_l2 :'))
+
+print('semester 3 :')
+
+s3_t1 = int(input('enter the mark of s3_t1 :'))
+s3_t2 = int(input('enter the mark of s3_t2 :'))
+s3_t3 = int(input('enter the mark of s3_t3 :'))
+s3_t4 = int(input('enter the mark of s3_t4 :'))
+s3_t5 = int(input('enter the mark of s3_t5 :'))
+s3_t6 = int(input('enter the mark of s3_t6 :'))
+s3_l1 = int(input('enter the mark of s3_l1 :'))
+s3_l2 = int(input('enter the mark of s3_l2 :'))
+
+print('semester 4 :')
+
+s4_t1 = int(input('enter the mark of s4_t1 :'))
+s4_t2 = int(input('enter the mark of s4_t2 :'))
+s4_t3 = int(input('enter the mark of s4_t3 :'))
+s4_t4 = int(input('enter the mark of s4_t4 :'))
+s4_t5 = int(input('enter the mark of s4_t5 :'))
+s4_t6 = int(input('enter the mark of s4_t6 :'))
+s4_l1 = int(input('enter the mark of s4_l1 :'))
+s4_l2 = int(input('enter the mark of s4_l2 :'))
+
+print('semester 5 :')
+
+s5_t1 = int(input('enter the mark of s5_t1 :'))
+s5_t2 = int(input('enter the mark of s5_t2 :'))
+s5_t3 = int(input('enter the mark of s5_t3 :'))
+s5_t4 = int(input('enter the mark of s5_t4 :'))
+s5_t5 = int(input('enter the mark of s5_t5 :'))
+s5_t6 = int(input('enter the mark of s5_t6 :'))
+s5_l1 = int(input('enter the mark of s5_l1 :'))
+s5_l2 = int(input('enter the mark of s5_l2 :'))
+
+print('semester 6 :')
+
+s6_t1 = int(input('enter the mark of s6_t1 :'))
+s6_t2 = int(input('enter the mark of s6_t2 :'))
+s6_t3 = int(input('enter the mark of s6_t3 :'))
+s6_t4 = int(input('enter the mark of s6_t4 :'))
+s6_t5 = int(input('enter the mark of s6_t5 :'))
+s6_t6 = int(input('enter the mark of s6_t6 :'))
+s6_l1 = int(input('enter the mark of s6_l1 :'))
+s6_l2 = int(input('enter the mark of s6_l2 :'))
+
+print('semester 7 :')
+
+s7_t1 = int(input('enter the mark of s7_t1 :'))
+s7_t2 = int(input('enter the mark of s7_t2 :'))
+s7_t3 = int(input('enter the mark of s7_t3 :'))
+s7_l1 = int(input('enter the mark of s7_l1 :'))
+s7_pp1 = int(input('enter the mark of s7_pp1 :'))
+
+
+print('semester 8 :')
+
+s8_t1 = int(input('enter the mark of s8_t1 :'))
+s8_t2 = int(input('enter the mark of s8_t2 :'))
+s8_pp2 = int(input('enter the mark of s8_pp2 :'))
+
+
+total =  s1_t1 + s1_t2 + s1_t3 + s1_t4 +s1_t5 + s1_t6 + s1_l1 + s1_l2 + s2_t1 + s2_t2 + s2_t3 + s2_t4 +s2_t5 + s2_t6 + s2_l1 + s2_l2 + s3_t1 + s3_t2 + s3_t3 + s3_t4 +s3_t5 + s3_t6 + s3_l1 + s3_l2 + s4_t1 + s4_t2 + s4_t3 + s4_t4 +s4_t5 + s4_t6 + s4_l1 + s4_l2 + s5_t1 + s5_t2 + s5_t3 + s5_t4 +s5_t5 + s5_t6 + s5_l1 + s5_l2 + s6_t1 + s6_t2 + s6_t3 + s6_t4 +s6_t5 + s6_t6 + s6_l1 + s6_l2 + s7_t1 + s7_t2 + s7_t3 + s7_l1 +s7_pp1 + s8_t1 + s8_t2 + s8_pp2 
+
+print('Total :' , total)
+
+gpa = total/56
+
+print('GPA :' , gpa)
+
+if ( s1_t1 , s1_t2 , s1_t3 , s1_t4 , s1_t5 , s1_t6 , s1_l1 , s1_l2 , s2_t1 , s2_t2 , s2_t3 , s2_t4 , s2_t5 , s2_t6 , s2_l1 , s2_l2 , s3_t1 , s3_t2 , s3_t3 , s3_t4 , s3_t5 , s3_t6 , s3_l1 , s3_l2 , s4_t1 , s4_t2 , s4_t3 , s4_t4 , s4_t5 , s4_t6 , s4_l1 , s4_l2 , s5_t1 , s5_t2 , s5_t3 , s5_t4 , s5_t5 , s5_t6 , s5_l1 , s5_l2 , s6_t1 , s6_t2 , s6_t3 , s6_t4 , s6_t5 , s6_t6 , s6_l1 , s6_l2 , s7_t1 , s7_t2 , s7_t3 , s7_l1 , s7_pp1 , s8_t1 , s8_t2 , s8_pp2 ):
+    print('Result : Pass')
+
+else:
+    print('Result : Fail')
+
+
+cgpa = gpa/8
+
+print('CGPA :' , cgpa)
+
+if cgpa >= 10.0:
+
+    print('GRADE : A , Graduated in 1st class')
+
+elif cgpa >= 6.0:
+
+    print('GRADE : B , Graduated in 2nd class')
+
+else:
+    print('No GRADE , RE-APPERENCE')
+
+
+
+
